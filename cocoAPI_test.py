dd46c62305a71c2ce8cae86ef1d8c6f8a6215418
ianctw16 <ian08160816@hotmail.com>
2017-11-22 14:10:50 +0800

    2

--- cocoAPI_test.py
+++ cocoAPI_test.py
@@ -6,6 +6,7 @@ cocoGT = COCO(ann_file)
 
 # getAnnIds(Give 0 imgIds return all annotations id)
 annIds = cocoGT.getAnnIds(iscrowd=None)
+# anns has all annotation data
 anns = cocoGT.loadAnns(annIds)
 
 '''
@@ -23,30 +24,32 @@ d.setdefault('boxes', [])
 d2.setdefault('images', [])
 d3.setdefault('ids', [])
 
-
+# store bbox in d['boxes']
 for i in range(0, len(anns)):
     d['boxes'].append(anns[i]['bbox'])
+# store image_id in d2['images']
+print(len(anns))
 for i in range(0, len(anns)):
     d2['images'].append(anns[i]['image_id'])
+    #  print(d2['images'])
+print(d2['images'][2412], d2['images'][0])
+# store file_name in d3['ids']
 for id in d2['images']:
-    d3['ids'].append(cocoGT.imgs[anns[i]['image_id']]['file_name'])
+    d3['ids'].append(cocoGT.imgs[anns[id]['image_id']]['file_name'])
 
-
+# trans d['boxes'] to numpy array
 image_labels = [np.array(boxes, dtype=np.uint16) for boxes in d['boxes']]
 image_labels = np.array(image_labels)
+# trans d3['ids'] --> file_name to numpy array
 images = [np.array(id) for id in d3['ids']]
 images = np.array(images)
+print(image_labels)
 print(images)
-print(type(image_labels))
-print(type(images))
-
 np.savez("my_data", images=images, boxes=image_labels)
 
-"""
 data = np.load('my_data.npz')
 
 image_data = data['images']
 boxes = data['boxes']
 print(image_data)
 print(boxes)
-"""
